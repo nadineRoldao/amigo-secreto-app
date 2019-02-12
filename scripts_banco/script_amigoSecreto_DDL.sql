163359ff34ac478e7647a7a64b712456a017c70f
nadineRoldao <vitorianadineroldao@gmail.com>
2019-02-12 16:37:02 -0200

    adicionado ddl do banco

---
+++ scripts_banco/script_amigoSecreto_DDL.sql
@@ -0,0 +1,41 @@
+drop database amigo_secreto_app;
+create database amigo_secreto_app;
+use amigo_secreto_app;
+
+drop table if exists amigo;
+drop table if exists grupo;
+drop table if exists grupo_amigo;
+drop table if exists grupo_postagem;
+
+create table amigo (
+    id    		int   			not null auto_increment primary key,
+    nome        varchar(255)    not null,
+    email       varchar(255)    not null,
+    senha       varchar(255)    not null,
+    imagem      varchar(255)    null
+);
+
+create table grupo (
+    id              int 		    not null auto_increment primary key,
+    nome            varchar(255)    not null,
+    id_amigo        int             null,
+    data_sorteio    timestamp       null,
+    valor_min       float           null,
+    valor_max       float           null
+);
+
+create table grupo_amigo (
+	id_amigo            int  		   	not null,
+    id_grupo            int    			not null,
+    id_amigo_sorteado   int             null,
+    ativo               ENUM('y', 'n')  not null default 'n',
+    primary key (id_grupo, id_amigo)
+);
+
+create table grupo_postagem (
+    id          int   		 not null auto_increment primary key,
+	id_amigo    int    		 not null,
+    id_grupo    int    		 not null,
+    texto       varchar(1000),
+    link        varchar(255)
+);
