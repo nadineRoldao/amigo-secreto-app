7df75117d4d5cf76ec4b019f0bfd49a7459a67ca
Nadine Roldao <vitorianadineroldao@gmail.com>
2019-02-26 22:26:51 -0300

    adicionado listagem de amigos por grupo

--- amigo-app-backend/controllers/grupoController.js
+++ amigo-app-backend/controllers/grupoController.js
@@ -13,17 +13,49 @@ module.exports = (app) => {
         });
     });
 
-    app.delete('/grupo/deletar/:id', (req, res) => {
+    app.delete('/grupo/:idGrupo/:idAmigo/deletar', (req, res) => {
         
-        let id = req.params.id;
+        let id = req.params.idGrupo;
+        let idAmigo = req.params.idAmigo;
         let conn = app.repositories.connectionFactory();
         let grupoRepository = new app.repositories.grupoRepository(conn);
         
-        grupoRepository.deletar(id, (err) => {
+        grupoRepository.deletar(id, idAmigo, (err) => {
             if(err){
              res.send('Houve um erro');
+             return;
             }
             res.send('SUCESSO!!');
+        });
+    });
+
+    app.get('/grupo/amigos-ativos/:id', (req, res) => {
+
+        let id = req.params.id;
+        let conn = app.repositories.connectionFactory();
+        let grupoRepository = new app.repositories.grupoRepository(conn);
+
+        grupoRepository.listarAmigosAtivos(id, (err, result) => {
+            if (err) {
+                res.send('Houve um erro' + err);
+                return;
+            }
+            res.send(result);
+        });
+    });
+
+    app.get('/grupo/amigos-inativos/:id', (req, res) => {
+
+        let id = req.params.id;
+        let conn = app.repositories.connectionFactory();
+        let grupoRepository = new app.repositories.grupoRepository(conn);
+
+        grupoRepository.listarAmigosInativos(id, (err, result) => {
+            if (err) {
+                res.send('Houve um erro' + err);
+                return;
+            }
+            res.send(result);
         });
     });
 }
--- amigo-app-backend/repositories/grupoRepository.js
+++ amigo-app-backend/repositories/grupoRepository.js
@@ -8,8 +8,16 @@ GrupoRepository.prototype.create = (grupo, callback) => {
     connection.query('INSERT INTO grupo SET ?', grupo, callback);
 }
 
-GrupoRepository.prototype.deletar = (id, callback) => {
-    connection.query('DELETE FROM grupo WHERE id = ?', [id], callback);
+GrupoRepository.prototype.deletar = (id, idAmigo, callback) => {
+    connection.query('DELETE FROM grupo WHERE id = ? and id_amigo = ?', [id, idAmigo], callback);
+}
+
+GrupoRepository.prototype.listarAmigosAtivos = (id, callback) => {
+    connection.query("SELECT ga.id_amigo, a.nome FROM amigo a INNER JOIN grupo_amigo ga ON a.id = ga.id_amigo WHERE ga.id_grupo = ? AND ga.ativo = 'y'", [id], callback);
+}
+
+GrupoRepository.prototype.listarAmigosInativos = (id, callback) => {
+    connection.query("SELECT ga.id_amigo, a.nome FROM amigo a INNER JOIN grupo_amigo ga ON a.id = ga.id_amigo WHERE ga.id_grupo = ? AND ga.ativo = 'n'", [id], callback);
 }
 
 module.exports = () => GrupoRepository;
